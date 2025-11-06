c765f81130e21c47c2866c494c205c51af911f23
Daria Zhdanok <knemune@gmail.com>
2025-11-06 22:44:26 +0300

    ДЗ3, point 4, зарегистрирована ячейка

--- dszhdanokPW2/WishStoringViewController.swift
+++ dszhdanokPW2/WishStoringViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 final class WishStoringViewController: UIViewController {
     
     private let table = TableConfiguration.CreateTable()
+    private var wishArray: [String] = ["I wish to add cells to the table"]
     
     override func viewDidLoad() {
         view.backgroundColor = .blue
         configureUI()
+        table.register(WrittenWishCell.self, forCellReuseIdentifier: WrittenWishCell.reuseId)
     }
     
     private func configureUI() {
@@ -26,10 +28,17 @@ final class WishStoringViewController: UIViewController {
 extension WishStoringViewController: UITableViewDataSource {
     //расширение протокола
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return wishArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: WrittenWishCell.reuseId,
+            for: indexPath
+        )
+        guard let wishCell = cell as? WrittenWishCell else { return cell }
+        wishCell.configure(with: wishArray[indexPath.row])
+        return wishCell
     }
 }
