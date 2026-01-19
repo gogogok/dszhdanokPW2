a7ffd5911444d3f67a8b8d14f6b97d95f30182ad
Daria Zhdanok <knemune@gmail.com>
2026-01-19 04:24:01 +0300

    PW4 - Point4 - Добавлена табличка которую пока не видно

--- dszhdanokPW2/WishCalendarViewController.swift
+++ dszhdanokPW2/WishCalendarViewController.swift
@@ -9,9 +9,75 @@ import UIKit
 
 final class WishCalendarViewController: UIViewController {
 
+    // MARK: - Constsnts
+    private enum Constants {
+        static let contentInset : UIEdgeInsets = .init(top: 16, left: 16, bottom: 16, right: 16)
+        static let collectionTop: CGFloat = 20
+    }
+    
+    // MARK: - Fields
+    private let collectionView: UICollectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewFlowLayout()
+    )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Wish Calendar"
+        configureCollection()
+    }
+    
+    private func configureCollection() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .white
+        collectionView.alwaysBounceVertical = true
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.contentInset = Constants.contentInset
+        
+        /* Temporary line */
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        view.addSubview(collectionView)
+        collectionView.pinHorizontal(to: view)
+        collectionView.pinBottom(to: view.safeAreaLayoutGuide.bottomAnchor)
+        collectionView.pinTop(to: view.safeAreaLayoutGuide.topAnchor, Constants.collectionTop)
+    }
+    
+}
+
+// MARK: - UICollectionViewDataSource
+extension WishCalendarViewController: UICollectionViewDataSource {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+        return 10
+    }
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension WishCalendarViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+// Adjust cell size as needed
+        return CGSize(width: collectionView.bounds.width - 10, height: 100)
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        didSelectItemAt indexPath: IndexPath
+    ) {
+        print("Cell tapped at index \(indexPath.item)")
     }
 }
